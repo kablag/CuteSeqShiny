832255f2d5d4024020e84a3eb4460e2c2e71ea52
kablag <k.blag@yandex.ru>
2017-04-21 15:01:35 +0300

    legend table update

--- generic.R
+++ generic.R
@@ -205,21 +205,20 @@ cuteSeq <- function(flatMap,
   legendTbl <- ""
   if (includeLegend) {
     legendTbl <-
-      sprintf("%s<br>%s%s%s",
-                htmlTable::htmlTable(seqPalette[!"Mismatch Color",
-                                                Color := sprintf("<span style='background-color: %s;'>ATGC</span>", color)]
-                  [!"Mismatch Color", Features := paste(unique(flatMapCopy[typeID == param, label]), collapse = ", "),
-                    by = param]
-                  [!"Mismatch Color", Strand :=  str_extract(param,"[-\\+]$")]
-                  [!"Mismatch Color", Param :=  str_replace(param,"[-\\+]$", "")]
-                  # [,c("param", "strand") = list(str_match(param, "(.*) ?([+-])$?"))]
-                  [!"Mismatch Color", .(Color, Param, Strand, Features)],
-                  rnames = FALSE, header = c("Color", "Group", "Strand", "Features"),
-                  align.header = paste(rep('c', 4), collapse = ''),
-                  align = paste(rep('c', 4), collapse = ''))
+      sprintf("%s%s%s%s",
+              shiny::renderTable(seqPalette[!"Mismatch Color",
+                                     Color := sprintf("<span style='background-color: %s;'>ATGC</span>", color)]
+                          [!"Mismatch Color", Features := paste(unique(flatMapCopy[typeID == param, label]), collapse = ", "),
+                            by = param]
+                          [!"Mismatch Color", Strand :=  str_extract(param,"[-\\+]$")]
+                          [!"Mismatch Color", Param :=  str_replace(param,"[-\\+]$", "")]
+                          # [,c("param", "strand") = list(str_match(param, "(.*) ?([+-])$?"))]
+                          [!"Mismatch Color", .(Color, Param, Strand, Features)],
+                          colnames = FALSE,
+                          sanitize.text.function = function(x) x)()
               ,
               ifelse(any(flatMapCopy[, mismatchHere] == TRUE),
-                     sprintf("<br><span style='background-color: %s'>&emsp;&emsp;</span>&emsp;Mismatch",
+                     sprintf("<span style='background-color: %s'>&emsp;&emsp;</span>&emsp;Mismatch",
                              seqPalette["Mismatch Color", color]),
                      ""),
               ifelse(any(flatMapCopy[, intersectionHere] == TRUE),
